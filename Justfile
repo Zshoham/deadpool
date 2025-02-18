1bb93641c30c54e8e7f103735e3d488faf008fa0
Zshoham <ShohamZarfti@gmail.com>
2025-02-18 21:46:14 +0200

    testing: arm and neon, with qemu in docker testing

--- Justfile
+++ Justfile
@@ -22,5 +22,20 @@ fuzz:
   CXX=clang CC=clang cmake -B build -DCMAKE_BUILD_TYPE=Debug .
   cmake --build ./build --target allocator_fuzz
 
+arm:
+  docker build -f arch/Dockerfile.arm -t dp-cross-arm:latest .
+  docker run -itd -w /mnt/deadpool -v $PWD:/mnt/deadpool --name dp_cross_arm dp-cross-arm:latest
+  docker exec -it dp_cross_arm bash -c "cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=arch/arm-toolchain.cmake"
+  docker exec -it dp_cross_arm bash -c "cmake --build ./build --target allocator_test"
+  docker exec -it dp_cross_arm bash -c "qemu-arm-static -L /usr/arm-linux-gnueabihf build/test/allocator_test"
+  docker rm -f dp_cross_arm
+
+neon-test:
+  docker build -f arch/Dockerfile.arm -t dp-cross-arm:latest .
+  docker run -itd -w /mnt/deadpool -v $PWD:/mnt/deadpool --name dp_cross_arm dp-cross-arm:latest
+  docker exec -it dp_cross_arm bash -c "arm-linux-gnueabihf-gcc -static -march=armv7 -mtune=cortex-a8 -mfpu=neon clz_test.c -o clz_test"
+  docker exec -it dp_cross_arm bash -c "qemu-arm-static clz_test"
+  docker rm -f dp_cross_arm
+
 clean:
   rm -rf ./build/
---
+++ clz_test.c
@@ -0,0 +1,50 @@
+// Type your code here, or load an example.
+#include <stdio.h>
+#include <stddef.h>
+#include <math.h>
+
+#include <arm_neon.h>
+
+void print_vec(uint32x4_t vec) {
+    uint32_t val0 = vgetq_lane_u32(vec, 0);
+    printf("Element 0: 0x%032b\n", val0);
+
+    uint32_t val1 = vgetq_lane_u32(vec, 1);
+    printf("Element 1: 0x%032b\n", val1);
+
+    uint32_t val2 = vgetq_lane_u32(vec, 2);
+    printf("Element 2: 0x%032b\n", val2);
+
+    uint32_t val3 = vgetq_lane_u32(vec, 3);
+    printf("Element 3: 0x%032b\n", val3);
+    printf("\n"); // Newline at the end
+
+}
+
+uint8_t n_clz(uint64_t* vec) {
+    int clz0 = __builtin_clzll(vec[0]);
+    if (clz0 > 0)
+        return clz0;
+    int clz1 = __builtin_clzll(vec[1]);
+    return 64 + clz1;
+}
+
+uint8_t v_clz(uint32x4_t vec){
+    uint32x4_t lzvec = vclzq_u32(vec);
+    uint32x2_t min_pair = vmin_u32(vget_low_u32(vec), vget_high_u32(vec));
+    uint32_t min = (vget_lane_u32(min_pair, 0) < vget_lane_u32(min_pair, 1)) ? vget_lane_u32(min_pair, 0) : vget_lane_u32(min_pair, 1);
+    return min;
+}
+
+int main() {
+    uint32x4_t bmap = {0xaabb,0xbbcc,0xccdd,0xeeff};
+    uint64_t vec[] = {0xaabbbbcc, 0xccddeeff};
+    printf("bmap values: \n");
+    print_vec(bmap);
+
+    printf("clz generic: %d\n", n_clz(vec));
+    printf("clz neon: %d\n", v_clz(bmap));
+
+    return 0;
+}
+
