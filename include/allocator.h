43a5b206254ed2f0368fa516446d7b4464cb96f7
Zshoham <ShohamZarfti@gmail.com>
2026-01-25 10:52:59 +0000

    feat: extract log definitions into a seperate header

--- include/allocator.h
+++ include/allocator.h
@@ -7,42 +7,10 @@
 
 #include <config_macros.h>
 
+#include "log.h"
+
 #ifdef __cplusplus
 extern "C" {
-#endif
-
-#if DP_LOG
-
-typedef struct dp_logger {
-  void (*debug)(const char *fmt, ...);
-  void (*info)(const char *fmt, ...);
-  void (*warning)(const char *fmt, ...);
-  void (*error)(const char *fmt, ...);
-} dp_logger;
-
-#define DP_DEBUG(alloc, ...)                                                                       \
-  do {                                                                                             \
-    alloc->logger.debug(__VA_ARGS__);                                                              \
-  } while (0)
-#define DP_INFO(alloc, ...)                                                                        \
-  do {                                                                                             \
-    alloc->logger.info(__VA_ARGS__);                                                               \
-  } while (0)
-#define DP_WARNING(alloc, ...)                                                                     \
-  do {                                                                                             \
-    alloc->logger.warning(__VA_ARGS__);                                                            \
-  } while (0)
-#define DP_ERROR(alloc, ...)                                                                       \
-  do {                                                                                             \
-    alloc->logger.error(__VA_ARGS__);                                                              \
-  } while (0)
-
-#else
-#define DP_DEBUG(alloc, ...)   /*Logging Disabled*/
-#define DP_INFO(alloc, ...)    /*Logging Disabled*/
-#define DP_WARNING(alloc, ...) /*Logging Disabled*/
-#define DP_ERROR(alloc, ...)   /*Logging Disabled*/
-
 #endif
 
 typedef struct block_header {
